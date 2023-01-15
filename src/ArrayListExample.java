cae7b11b4e454f4d7c94d1839dfb70da84a44547
Mohana Venkatraman <mohanadesi@gmail.com>
2023-01-15 23:18:19 +0530

    first commit

---
+++ src/ArrayListExample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io;
+
+public class ArrayListExample {
+	public static void main(String args[]) {
+		ArrayList<String> obj = new ArrayList<String>();
+		obj.add("Ajith");
+		obj.add("Vijay");
+		obj.add(1, "Surya");
+		obj.add(0, "Rahul");
+		System.out.println("Currently the array list has the following objects :"+obj);
+		
+		
+		
+	}
+
+}

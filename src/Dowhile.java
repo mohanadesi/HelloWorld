f4bb4bad26cc96133851bcbfd3462b9044ba1b4c
Mohana Venkatraman <mohanadesi@gmail.com>
2023-01-28 22:53:30 +0530

    Create Dowhile.java

---
+++ src/Dowhile.java
@@ -0,0 +1,23 @@
+
+public class Dowhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 // Declaring and initialization expression
+        int i = 1;
+ 
+        // Do-while loop
+        do {
+ 
+            // Body of do-while loop
+            // Print statement
+            System.out.println("Hello World");
+ 
+            // Update expression
+            i++;
+        }
+        while (i < 6);
+    
+	}
+
+}

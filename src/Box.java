d51e0dccc369444a7a0601e46b043e5407e51e0e
Mohana Venkatraman <mohanadesi@gmail.com>
2023-02-02 16:25:40 +0530

    program for class and object example

---
+++ src/Box.java
@@ -0,0 +1,9 @@
+
+public class Box {
+	double length;
+	double width;
+	double height;
+	
+
+}
+// main class is in MyBox.java
---
+++ src/MyBox.java
@@ -0,0 +1,19 @@
+
+public class MyBox {
+
+	public static void main(String[] args) {
+		double volume;
+		// TODO Auto-generated method stub
+		Box b1 = new Box();
+		b1.length = 10;
+		b1.width = 20;
+		b1.height = 30;
+		volume = (b1.length * b1.width * b1.height);
+		System.out.println("Volume of my box " +volume);
+		
+;
+		
+	}
+
+}
+//called from class Box

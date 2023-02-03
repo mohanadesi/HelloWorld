3efb81c88fcc056f4b44f57ca5e860b6879181f1
Mohana Venkatraman <mohanadesi@gmail.com>
2023-02-03 12:01:46 +0530

    arraysdemo commit

---
+++ src/Arraysdemo.java
@@ -0,0 +1,18 @@
+
+public class Arraysdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int month_days[]= {10,20,30,40,50,60};
+		/* month_days=new int[4];
+		month_days[0]=10;
+		month_days[1]=20;
+		month_days[2]=30;
+		month_days[3]=40; */
+		for (int i=0;i<month_days.length;i++)
+		
+		System.out.println(month_days[i]);
+		
+	}
+
+}
